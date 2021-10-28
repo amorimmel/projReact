edce2348196282462c39edb32b7184976856f61b
Melissa Amorim <melissa18.mo@gmail.com>
2021-10-27 23:41:18 -0300

    sequencia do exercicio sobre react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import './App.css';
+
 
 function App() {
   return (
-    <Home/>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
