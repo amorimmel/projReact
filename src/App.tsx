47331295a95fb0d571dfd26f0a93713f3586f330
Melissa Amorim <melissa18.mo@gmail.com>
2021-10-28 20:55:17 -0300

    inserindo rotas e refatorando arquivos de estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './componentes/estaticos/navbar/Navbar';
 import Footer from './componentes/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
+import Login from './paginas/login/Login';
+
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+        <Switch>
+        <div style={{minHeight: '100vh'}}>
+
+          <Route exact path='/'>
+              <Login />
+            </Route>
+
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+          </div>
+        </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
