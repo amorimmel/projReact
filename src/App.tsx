84aa2ba1a0b53fce9879b13c72aefd71f6a02f11
Melissa Amorim <melissa18.mo@gmail.com>
2021-11-09 23:37:23 -0300

    Criando os componentes Tema e Postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,52 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Navbar from './componentes/estaticos/navbar/Navbar';
-import Footer from './componentes/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import './App.css';
-import Login from './paginas/login/Login';
-import CadastroUsuario from './paginas/cadastroUsuario/cadastroUsuario';
+  import React from 'react';
+  import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+  import Navbar from './componentes/estaticos/navbar/Navbar';
+  import Footer from './componentes/estaticos/footer/Footer';
+  import Home from './paginas/home/Home';
+  import './App.css';
+  import Login from './paginas/login/Login';
+  import CadastroUsuario from './paginas/cadastroUsuario/cadastroUsuario';
+  import ListaTema from './componentes/temas/listatema/ListaTema';
+  import ListaPostagem from './componentes/postagens/listapostagem/ListaPostagem';
 
 
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-        <Switch>
-        <div style={{minHeight: '100vh'}}>
+  function App() {
+    return (
+      <Router>
+        <Navbar />
+          <Switch>
+          <div style={{minHeight: '100vh'}}>
 
-          <Route exact path='/'>
-              <Login />
-            </Route>
+            <Route exact path='/'>
+                <Login />
+              </Route>
 
-            <Route path='/login'>
-              <Login />
-            </Route>
+              <Route path='/login'>
+                <Login />
+              </Route>
 
-            <Route path='/home'>
-              <Home />
-            </Route>
+              <Route path='/home'>
+                <Home />
+              </Route>
 
-            <Route path='/cadastrousuario'>
-              <CadastroUsuario />
-            </Route>
+              <Route path='/cadastrousuario'>
+                <CadastroUsuario />
+              </Route>
 
-          </div>
-        </Switch>
-      <Footer />
-    </Router>
-  );
-}
+              <Route path='/temas'>
+                <ListaTema />
+              </Route>
+              
+              <Route path='/posts'>
+                <ListaPostagem />
+              </Route>
 
-export default App;
+            </div>
+          </Switch>
+        <Footer />
+      </Router>
+    );
+  }
+
+  export default App;
